e0b4926de0d3637b9d8892e34fbfa7fc7594e8c6
ito78x <127704051+ito78x@users.noreply.github.com>
2023-06-10 22:42:38 -0300

    Add admin options

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+
 from .models import Book
 
 # Register your models here.
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    #readonly_fields = ("slug",)
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "rating",)
+    list_display = ("title", "author",)
+
+admin.site.register(Book, BookAdmin)
